2520435273a512046784a6eafddcb91bdd65fcd3
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2023-03-27 23:58:35 +0300

    gogo

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # node-js-project
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list" - https://prnt.sc/fuimDXcKmJnV
+
+# Отримуємо контакт по id
+
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6 - https://prnt.sc/qQ5m7yvwvbvk
+
+# Додаємо контакт
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22 - https://prnt.sc/Q7Uajb4vj23t
+
+# Видаляємо контакт
+
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH - https://prnt.sc/eTYZOR0CAgzV
---
+++ index.js
@@ -0,0 +1,41 @@
+const { listContacts, getContactById, addContact, removeContact } = require('./db/contacts')
+const { Command } = require('commander')
+const program = new Command()
+program
+	.option('-a, --action <type>', 'choose action')
+	.option('-i, --id <type>', 'user id')
+	.option('-n, --name <type>', 'user name')
+	.option('-e, --email <type>', 'user email')
+	.option('-p, --phone <type>', 'user phone')
+
+program.parse(process.argv)
+
+const argv = program.opts()
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+	switch (action) {
+		case 'list':
+			const allContacts = await listContacts()
+			console.log(allContacts)
+			break
+
+		case 'get':
+			const contact = await getContactById(id)
+			console.log(contact)
+			break
+
+		case 'add':
+			const newContact = await addContact(name, email, phone)
+			console.log(newContact)
+			break
+
+		case 'remove':
+			const contactToDel = await removeContact(id)
+			console.log(contactToDel)
+			break
+
+		default:
+			console.warn('\x1B[31m Unknown action type!')
+	}
+}
+invokeAction(argv)
